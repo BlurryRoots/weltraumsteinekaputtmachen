afad480387f0178eadc70d90582797bb594fc803
blurryroots <blurryroots@posteo.de>
2015-02-04 00:31:47 +0100

    removed debug messages and fixed bg dimensions
    removed the debug log for mousekey events
    fixed formular to calculate scale factor for background
    image depending on the current window size

--- src/Game.lua
+++ src/Game.lua
@@ -29,12 +29,12 @@ function Game:Game ()
 	self.eventManager:subscribe ("MouseButtonUpEvent", self)
 	self.eventManager:subscribe ("ResizeEvent", self)
 
-  self.bg = love.graphics.newImage("gfx/bg.jpg")
+	self.bg = love.graphics.newImage("gfx/bg.jpg")
 
 	self.log = {}
 	self.shipoflife = Ship ()
-  self.eventManager:subscribe ("KeyboardKeyDownEvent", self.shipoflife)
-  self.eventManager:subscribe ("KeyboardKeyUpEvent", self.shipoflife)
+	self.eventManager:subscribe ("KeyboardKeyDownEvent", self.shipoflife)
+	self.eventManager:subscribe ("KeyboardKeyUpEvent", self.shipoflife)
 end
 
 -- Raises (queues) a new event
@@ -46,15 +46,6 @@ end
 -- Callback used by EventManager
 function Game:handle (event)
 	--
-	print ("trying to handle " .. type (event))
-	if event:getClass () == "MouseButtonUpEvent" then
-		table.insert (self.log, {
-			timestamp = tostring (os.date ("%c")),
-			ttl = 4,
-			tal = 0,
-			message = tostring (event)
-		})
-	end
 end
 
 -- Updates game logic
@@ -62,45 +53,21 @@ function Game:onUpdate (dt)
 	--
 	self.eventManager:update (dt)
 	self.shipoflife:onUpdate (dt)
-
-	local toberemoved = {}
-	for i, v in pairs (self.log) do
-		if (v.ttl - v.tal) <= 0 then
-			table.insert (toberemoved, i)
-		else
-			v.tal = v.tal + dt
-		end
-	end
-	for i, v in pairs (toberemoved) do
-		table.remove (self.log, v)
-	end
 end
 
 -- Renders stuff onto the screen
 function Game:onRender ()
 	--
-  local width = love.graphics.getWidth()
-  local height = love.graphics.getHeight()
-  local scaleX = self.bg:getWidth() / width
-  local scaleY = self.bg:getHeight() / height
-  
-  love.graphics.draw(self.bg, 0, 0, 0, scaleX, scaleY)
-	self:renderLog ()
+	local width = love.graphics.getWidth()
+	local height = love.graphics.getHeight()
+	local scaleX = width / self.bg:getWidth()
+	local scaleY = height / self.bg:getHeight()
+	love.graphics.draw(self.bg, 0, 0, 0, scaleX, scaleY)
+
 	self.shipoflife:onRender ()
-  
 end
 
 -- Gets called when game exits. May be used to do some clean up.
 function Game:onExit ()
 	--
-end
-
-function Game:renderLog ()
-	--
-	local basepos = {x = 42, y = 42}
-	for i, v in pairs (self.log) do
-		local msg = "[" .. v.timestamp .. "]: " .. v.message
-		local y = basepos.y + (i - 1) * 21
-		love.graphics.print (msg, basepos.x, y)
-	end
 end
