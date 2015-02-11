bf06e7fc386e6a7e5686f94b04c703bfd3232cda
blurryroots <blurryroots@posteo.de>
2015-02-11 15:48:16 +0100

    entity manager
    entity manager can now be used to create entities, as well
    as add remove and get data from it

--- lib/yanecos/EntityManager.lua
+++ lib/yanecos/EntityManager.lua
@@ -2,22 +2,78 @@ require ("lib.lclass")
 
 class "EntityManager"
 
-function EntityManager:EntityManager (datebase)
-	if not database then
-		error ("EntityManager needs a database!")
+function EntityManager:EntityManager ()
+	-- last assigned id
+	self.idCounter = 0
+	-- tags -> list of ids
+	self.tags = {}
+	-- id -> list of assigned data
+	self.data = {}
+end
+
+function EntityManager:nextId ()
+	self.idCounter = self.idCounter + 1
+
+	return self.idCounter
+end
+
+function EntityManager:createEntity (tags)
+	-- fetch newly created id (no need for reuse lua has bignum (whoohoo :D))
+	local eid = self:nextId ()
+	-- create empty data list for entity
+	self.data[eid] = {}
+	-- add tags if any
+	if tags then
+		for _, tag in pairs (tags) do
+			self:addTag (eid, tag)
+		end
 	end
-	self.database = database
+
+	return eid
 end
 
-function EntityManager:CreateEntity ()
-	--
+function EntityManager:addTag (eid, tag)
+	if not self.tags[tag] then
+		self.tags[tag] = {}
+	end
+
+	table.insert (self.tags[tag], eid)
 end
 
-function EntityManager:AddData (eid, data)
+function EntityManager:addData (eid, data)
+	if self:hasData (eid, data) then
+		error ("Entity " .. eid .. " already has data of type " .. data.__type)
+	end
+
+	table.insert (self.data[eid], data)
 end
 
-function EntityManager:RemoveData (eid, datatype)
+function EntityManager:removeData (eid, datatype)
+	if not self:hasData (eid, data) then
+		error ("Entity " .. eid .. " has no data of type " .. data.__type)
+	end
 end
 
-function EntityManager:GetData (eid, datatype)
+function EntityManager:hasData (eid, datatype)
+	if not self.data[eid] then
+		error ("There is no entity with id " .. eid)
+	end
+
+	local has = false
+	for _, data in pairs (self.data[eid]) do
+		has = data.__type == datatype.__type
+		if has then
+			break
+		end
+	end
+
+	return has
+end
+
+function EntityManager:getData (eid, datatype)
+	if not self.data[eid] then
+		error ("There is no entity with id " .. eid)
+	end
+
+	return self.data[eid]
 end
